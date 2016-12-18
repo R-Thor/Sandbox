8b3e10b8b4d99e4c33fc1ff415fa957b240be79a
R-Thor <swaybo@yahoo.com>
2016-12-18 12:07:21 -0500

    tic-tac-clo winner? refactoring

--- Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
+++ Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
@@ -1,15 +1,82 @@
 (ns tic-tac-clo.core
   (:gen-class))
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-(def game-board (zipmap (range 9) (repeat 9 "#")))
-(def players ["O" "X"])
-(def win-conditions [[0 1 2][3 4 5][6 7 8][0 3 6][1 4 7][2 5 8][0 4 8][2 4 6]])
-(def square-values (frequencies (flatten win-conditions)))
-(defn squares-available [board] (keys (filter (fn [[k v]](= "#" v)) board)))
-(defn square-distribution [board winconditions]
- (let [square-occupiers (distinct(vals board))]
-   square-occupiers))
-;(defn 
+  ;-----------------------------------------------------------------------
+  ;immutables
+  ;-----------------------------------------------------------------------
+  (def game-board (zipmap (range 9) (repeat 9 nil)))
+  (def players ["O" "X"])
+  (def win-conditions [[0 1 2][3 4 5][6 7 8][0 3 6][1 4 7][2 5 8][0 4 8][2 4 6]])
+  (def square-values (frequencies (flatten win-conditions)))
+  
+  (defn squares-available [board] (keys (filter (fn [[k v]](= nil v)) board)))
+  (defn square-distribution [board] 
+    (let 
+	  [
+	    squareoccupiers (distinct(vals board))
+	  ]
+	  (zipmap squareoccupiers (for [x squareoccupiers] (keys (filter (fn [[k v]] (= v x)) board))))
+	)
+   )
+   (defn remove#predicate [examined-space] (= examined-space nil))
+
+   (defn -scoreable-squares [board win-condition] (count(remove remove#predicate ( vals (select-keys board win-condition)))))
+
+   (defn -scoreing-player-variability [board win-condition] (count(distinct( vals (select-keys board win-condition)))))
+
+   (defn win-event? [board win-condition] (and (= (-scoreable-squares win-condition) (count win-condition)) (= (-scoreing-player-variability win-condition) 1)))
+   
+   ;(defn winner? [board winconditions])
+    ;-----------------------------------------------------------------------
+  ;mutables
+  ;-----------------------------------------------------------------------
+  ;(defn set-square [board player space] (update 
+  ;-----------------------------------------------------------------------
+  ;run
+  ;-----------------------------------------------------------------------
+  (defn game-loop 
+   [
+    init-game-board
+    init-players
+    init-win-conditions
+    init-square-values
+   ]
+   (do
+     (println init-game-board)
+	 (println init-players)
+	 (println init-win-conditions)
+	 (println init-square-values)
+	 (take 9 (cycle init-players))
+	 (println (squares-available init-game-board))
+	 (println (rand-nth (into [] (squares-available init-game-board))))
+	 (println (assoc init-game-board (rand-nth (into [] (squares-available init-game-board))) "X"))
+	 ;
+   ))
+  (defn -main
+    "I don't do a whole lot ... yet."
+    [& args]
+    (println "Hello, World!")
+	(game-loop game-board players win-conditions square-values)
+  )
+
+  
+     ;(defn winner? [board winconditions]
+   ;  (for [wincondition winconditions]
+	;   (let 
+	;     [
+	;       xocount (count(remove remove#predicate ( vals (select-keys board wincondition))))
+	;  	   distinctcount (count(distinct( vals (select-keys board wincondition))))
+	;     ]
+	;	 (and (= xocount 3) (= distinctcount 1))
+	;   )
+	; )
+   ;)
+   ;(defn resolve-win-condition [board wincondition]
+   ;  (let 
+	;    [
+	;       xocount (count(remove remove#predicate ( vals (select-keys board wincondition))))
+	;  	   distinctcount (count(distinct( vals (select-keys board wincondition))))
+	;    ]
+	;	(and (= xocount (count wincondition) (= distinctcount 1)))
+	;  )
+   ; )
+   
--- Clojurebox/tic-tac-clo/test/tic_tac_clo/core_test.clj
+++ Clojurebox/tic-tac-clo/test/tic_tac_clo/core_test.clj
@@ -2,3 +2,7 @@
   (:require [clojure.test :refer :all]
             [tic-tac-clo.core :refer :all]))
 (deftest tictactest (is (= true (= 1 1))))
+;(deftest game-board-test [board] (=is (= board {0 nil, 1 nil, 2 nil, 3 nil, 4 nil, 5 nil, 6 nil, 7 nil, 8 nil})))
+(deftest winner?-test (let [board {0 \X 1 \X 2 \X} winconditions [[0 1 2]]] (winner? board winconditions)))
+;(deftest game-board-test [board] (=is (= board {0 nil, 1 nil, 2 nil, 3 nil, 4 nil, 5 nil, 6 nil, 7 nil, 8 nil})))
+;(defn winner? [board winconditions]
