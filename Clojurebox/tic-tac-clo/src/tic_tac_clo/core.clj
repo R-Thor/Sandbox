350ded5f702d97a9ca84f1737da0f24ddb6f9fdc
R-Thor <swaybo@yahoo.com>
2016-12-18 19:13:34 -0500

    tic-tac-clo testing added to refactoring and completed winner?

--- Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
+++ Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
@@ -1,57 +1,56 @@
 (ns tic-tac-clo.core
   (:gen-class))
-  ;-----------------------------------------------------------------------
-  ;immutables
-  ;-----------------------------------------------------------------------
-  (def game-board (zipmap (range 9) (repeat 9 nil)))
-  (def players ["O" "X"])
-  (def win-conditions [[0 1 2][3 4 5][6 7 8][0 3 6][1 4 7][2 5 8][0 4 8][2 4 6]])
-  (def square-values (frequencies (flatten win-conditions)))
-  
-  (defn squares-available [board] (keys (filter (fn [[k v]](= nil v)) board)))
-  (defn square-distribution [board] 
-    (let 
-	  [
-	    squareoccupiers (distinct(vals board))
-	  ]
-	  (zipmap squareoccupiers (for [x squareoccupiers] (keys (filter (fn [[k v]] (= v x)) board))))
+	;-----------------------------------------------------------------------
+	;immutables
+	;-----------------------------------------------------------------------
+	(def game-board (zipmap (range 9) (repeat 9 nil)))
+	(def players ["O" "X"])
+	(def win-conditions [[0 1 2][3 4 5][6 7 8][0 3 6][1 4 7][2 5 8][0 4 8][2 4 6]])
+	(def square-values (frequencies (flatten win-conditions)))
+	(defn squares-available [board] (keys (filter (fn [[k v]](= nil v)) board)))
+	(defn square-distribution [board] 
+		(let 
+			[squareoccupiers (distinct(vals board))]
+			(zipmap squareoccupiers (for [x squareoccupiers] (keys (filter (fn [[k v]] (= v x)) board))))
+		)
 	)
-   )
-   (defn remove#predicate [examined-space] (= examined-space nil))
+	(defn remove#predicate [examined-space] (= examined-space nil))
+	(defn -scoreable-square-count-in-wincondition  [board wincondition] (count(remove remove#predicate ( vals (select-keys board wincondition)))))
+	(defn -scoreing-player-variability [board win-condition] (count(distinct( vals (select-keys board win-condition)))))
+	(defn win-event? [board win-condition] (and (= (-scoreable-square-count-in-wincondition board win-condition) (count win-condition)) (= (-scoreing-player-variability board win-condition) 1)))
+	(defn winner? [board winconditions]
+		(some (fn [wc] (win-event? board wc)) winconditions)
+	)
 
-   (defn -scoreable-squares [board win-condition] (count(remove remove#predicate ( vals (select-keys board win-condition)))))
-
-   (defn -scoreing-player-variability [board win-condition] (count(distinct( vals (select-keys board win-condition)))))
-
-   (defn win-event? [board win-condition] (and (= (-scoreable-squares win-condition) (count win-condition)) (= (-scoreing-player-variability win-condition) 1)))
-   
-   ;(defn winner? [board winconditions])
     ;-----------------------------------------------------------------------
-  ;mutables
-  ;-----------------------------------------------------------------------
-  ;(defn set-square [board player space] (update 
-  ;-----------------------------------------------------------------------
-  ;run
-  ;-----------------------------------------------------------------------
-  (defn game-loop 
-   [
-    init-game-board
-    init-players
-    init-win-conditions
-    init-square-values
-   ]
-   (do
-     (println init-game-board)
-	 (println init-players)
-	 (println init-win-conditions)
-	 (println init-square-values)
-	 (take 9 (cycle init-players))
-	 (println (squares-available init-game-board))
-	 (println (rand-nth (into [] (squares-available init-game-board))))
-	 (println (assoc init-game-board (rand-nth (into [] (squares-available init-game-board))) "X"))
-	 ;
-   ))
-  (defn -main
+	;mutables
+	;-----------------------------------------------------------------------
+	;(defn set-square [board player space] (update 
+	;-----------------------------------------------------------------------
+	;run
+	;-----------------------------------------------------------------------
+	(defn game-loop 
+		[
+			init-game-board
+			init-players
+			init-win-conditions
+			init-square-values
+		]
+		(do
+			;(println init-game-board)
+			;(println init-players)
+			;(println init-win-conditions)
+			;(println init-square-values)
+			
+			(println (take 9 (cycle init-players)))
+			
+			;(println (squares-available init-game-board))
+			;(println (rand-nth (into [] (squares-available init-game-board))))
+			;(println (assoc init-game-board (rand-nth (into [] (squares-available init-game-board))) "X"))
+		)
+	)
+
+	(defn -main
     "I don't do a whole lot ... yet."
     [& args]
     (println "Hello, World!")
--- Clojurebox/tic-tac-clo/test/tic_tac_clo/core_test.clj
+++ Clojurebox/tic-tac-clo/test/tic_tac_clo/core_test.clj
@@ -1,8 +1,43 @@
 (ns tic-tac-clo.core-test
   (:require [clojure.test :refer :all]
             [tic-tac-clo.core :refer :all]))
-(deftest tictactest (is (= true (= 1 1))))
-;(deftest game-board-test [board] (=is (= board {0 nil, 1 nil, 2 nil, 3 nil, 4 nil, 5 nil, 6 nil, 7 nil, 8 nil})))
-(deftest winner?-test (let [board {0 \X 1 \X 2 \X} winconditions [[0 1 2]]] (winner? board winconditions)))
-;(deftest game-board-test [board] (=is (= board {0 nil, 1 nil, 2 nil, 3 nil, 4 nil, 5 nil, 6 nil, 7 nil, 8 nil})))
-;(defn winner? [board winconditions]
+;(deftest tictactest (is (= true (= 1 1))))
+(deftest scoreable-square-count-in-wincondition-test 
+	(is (= 1 (-scoreable-square-count-in-wincondition {0 nil 1 nil 2 \X} [0 1 2])))
+	(is (= 2 (-scoreable-square-count-in-wincondition {0 nil 1 \X 2 \X}  [0 1 2])))
+	(is (= 3 (-scoreable-square-count-in-wincondition {0 \X 1 \X 2 \X}   [0 1 2])))
+)
+(deftest squares-available-test 
+	(is (= 1 (count (squares-available {0 nil}))))
+	(is (= 2 (count (squares-available {0 nil 1 nil}))))
+)
+(deftest square-distribution-1-test 
+	(is (= 1 (count (square-distribution {0 nil}))))
+	(is (= 2 (count (square-distribution {0 nil 1 \X}))))
+)
+(deftest remove#predicate-test 
+	(is (= true (remove#predicate nil)))
+	(is (= false (remove#predicate \X)))
+)
+(deftest -scoreable-square-count-in-wincondition-test 
+	(is (= 1 (-scoreable-square-count-in-wincondition {0 \X 1 \X 2 \X} [0 3 4])))
+	(is (= 2 (-scoreable-square-count-in-wincondition {0 \X 1 \X 2 \X} [0 1 4])))
+	(is (= 3 (-scoreable-square-count-in-wincondition {0 \X 1 \X 2 \X} [0 1 2])))
+)
+(deftest -scoreing-player-variability-test 
+	(is (= 1 (-scoreing-player-variability {0 \X 1 \X 2 \X} [0 1 2])))
+	(is (= 2 (-scoreing-player-variability {0 \X 1 \X 2 \O} [0 1 2])))
+	(is (= 3 (-scoreing-player-variability {0 \X 1 \Y 2 \Z} [0 1 2])))
+)
+
+(deftest win-event?-test 
+	(is (= false (win-event? {0 \X 1 \X 2 \O} [0 1 2])))
+	(is (= true  (win-event? {0 \X 1 \X 2 \X} [0 1 2])))
+)
+(deftest winner?-test 
+	(is (= false (winner? {0 \X 1 \X 2 \O} [[0 1 2] [3 4 5]])))
+	(is (= true  (winner? {0 \X 1 \X 2 \X} [[0 1 2] [3 4 5]])))
+)
+(deftest game-loop-test
+	(game-loop game-board players win-conditions square-values)
+)
