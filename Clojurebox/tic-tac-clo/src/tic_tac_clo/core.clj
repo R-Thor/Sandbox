7d38efa11b866707e81576a93f677d5feae40291
R-Thor <swaybo@yahoo.com>
2016-12-21 00:17:11 -0500

    tic-tac-clo: 1) Fixed a build error due to commented out line 2) working on comments

--- Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
+++ Clojurebox/tic-tac-clo/src/tic_tac_clo/core.clj
@@ -8,14 +8,25 @@
 	(def player-rotation (take 9 (cycle players)))
 	(def win-conditions [[0 1 2][3 4 5][6 7 8][0 3 6][1 4 7][2 5 8][0 4 8][2 4 6]])
 	(def square-values (frequencies (flatten win-conditions)))
-	(defn squares-available [board] (keys (filter (fn [[k v]](= nil v)) board)))
-	(defn square-distribution [board] 
+	
+	(defn squares-available
+		"takes a board and returns a sequence of unused (empty/nil) spaces."
+		[board] 
+		(keys (filter (fn [[k v]](= nil v)) board))
+	)
+	
+	(defn square-distribution 
+		"" 
+		[board] 
 		(let 
 			[squareoccupiers (distinct(vals board))]
 			(zipmap squareoccupiers (for [x squareoccupiers] (keys (filter (fn [[k v]] (= v x)) board))))
 		)
 	)
-	(defn remove#predicate [examined-space] (= examined-space nil))
+	(defn remove#predicate 
+		"Simple predicate function for checking to see if a grid space is nil (empty)" 
+		[examined-space] (= examined-space nil)
+	)
 	(defn -scoreable-square-count-in-wincondition  [board wincondition] (count(remove remove#predicate ( vals (select-keys board wincondition)))))
 	(defn -scoreing-player-variability [board win-condition] (count(distinct( vals (select-keys board win-condition)))))
 	(defn win-event? [board win-condition] (and (= (-scoreable-square-count-in-wincondition board win-condition) (count win-condition)) (= (-scoreing-player-variability board win-condition) 1)))
@@ -39,7 +50,7 @@
 		(-(int (last(into () (str somestrval))))48)
 	)
 
-    ;-----------------------------------------------------------------------
+	;-----------------------------------------------------------------------
 	;mutables
 	;-----------------------------------------------------------------------
 	;-----------------------------------------------------------------------
@@ -70,11 +81,10 @@
 					-stalemate 	(= true (stalemate? -gameboard))
 				]
 				(when (= true -win)(println "win"))
-				;(when (or (not= true -win) (not= true -stalemate))
 				(recur
 					-gameboard
 					(rest playerrotation)
-				))
+				)
 			)
 		)
 	)
