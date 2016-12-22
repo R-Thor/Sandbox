b392d24d7d33378b86fef697e2e54da43c7fbe46
R-Thor <swaybo@yahoo.com>
2016-12-21 21:26:01 -0500

    tic-tac-clo: should have missing files from last push + runnable from lein run

---
+++ Clojurebox/tic-tac-clo/src/tic_tac_clo/ai.clj
@@ -0,0 +1,3 @@
+(ns tic-tac-clo.a1
+  (:gen-class))
+(def square-values (frequencies (flatten win-conditions)))
---
+++ Clojurebox/tic-tac-clo/src/tic_tac_clo/misc.clj
@@ -0,0 +1,39 @@
+(ns tic-tac-clo.misc
+  (:gen-class))
+	(defn squares-available
+		"takes a board and returns a sequence of unused (empty/nil) spaces."
+		[board] 
+		(keys (filter (fn [[k v]](= nil v)) board))
+	)
+	(defn square-distribution 
+		"" 
+		[board] 
+		(let 
+			[squareoccupiers (distinct(vals board))]
+			(zipmap squareoccupiers (for [x squareoccupiers] (keys (filter (fn [[k v]] (= v x)) board))))
+		)
+	)
+	(defn default-getplayer-moves [input]
+		(let [user-entry input]
+			(loop [result input]
+				(if-not (or (not= "quit" result) nil)
+					result
+					(recur (read-line))
+				)
+			)
+			;input
+		)
+	)
+	(defn print-board-default [board]
+		(do
+			(doseq [spc board]
+				(do 
+					(print (val spc))
+					(when 
+						(= 0 (mod (inc (key spc)) (int (Math/sqrt (count board)))))
+						(print "\n")(flush)
+					)
+				)
+			)
+		)
+	)
