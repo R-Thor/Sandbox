6b7a8f2a496eb55cba3bc49a80d6247e1d93dd1f
R-Thor <swaybo@yahoo.com>
2017-01-01 16:36:17 -0500

    email-cleaner: added more wrapper code

--- Clojurebox/email-cleaner/src/email_cleaner/javax_mail_interop.clj
+++ Clojurebox/email-cleaner/src/email_cleaner/javax_mail_interop.clj
@@ -13,3 +13,9 @@
 [props]
 (let [session (. Session getInstance props nil)] session))
 (defn store-get [session] (.getStore session))
+(defn folder-get [store folder] (.getFolder folder))
+(defn folder-open [folder mode](.open mode))
+(defn folder-message-count [folder] (.getMessageCount folder))
+(defn folder-message-get [folder index] (.getMessage folder))
+(defn message-from-addresses [message] (.getFrom message))
+(defn message-get-subject [message] (.getSubject message))
